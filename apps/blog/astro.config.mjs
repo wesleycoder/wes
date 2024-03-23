1df1cc3521c04966ee8872b332983473eb1c08c7
Wésley Guimarães <wesley.coder@gmail.com>
2024-03-23 18:10:53 -0300

    feat: vercel adapter

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()],
+  adapter: vercel(),
 })
