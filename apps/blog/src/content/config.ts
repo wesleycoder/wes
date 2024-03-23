d9d420ffac799136a5e9ba79d397203b7c0ab223
Wésley Guimarães <wesley.coder@gmail.com>
2024-03-23 18:02:05 -0300

    feat: base dark-theme and draft content

--- apps/blog/src/content/config.ts
+++ apps/blog/src/content/config.ts
@@ -1,16 +1,23 @@
 import { defineCollection, z } from 'astro:content'
 
+const postSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date().optional(),
+  heroImage: z.string().optional(),
+})
+
+const draft = defineCollection({
+  type: 'content',
+  schema: postSchema,
+})
+
 const blog = defineCollection({
   type: 'content',
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
+  schema: postSchema.extend({
+    publishedAt: z.coerce.date(),
   }),
 })
 
-export const collections = { blog }
+export const collections = { blog, draft }
