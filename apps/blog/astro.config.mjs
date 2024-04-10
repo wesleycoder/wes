399b4defeff407019464127014b34ddf7d6cd8d4
Wésley Guimarães <wesley.coder@gmail.com>
2024-04-10 19:08:11 -0300

    feat(blog): i18n routes + styles

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -1,15 +1,24 @@
 import mdx from '@astrojs/mdx'
+import node from '@astrojs/node'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
 import { defineConfig } from 'astro/config'
 
+import compressor from 'astro-compressor'
+
+const adapter = process.argv.includes('--node') ? node({ mode: 'standalone' }) : vercel()
+
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()],
-  adapter: vercel(),
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'pt-br'],
+  },
+  integrations: [mdx(), sitemap(), tailwind(), process.env.NODE_ENV === 'production' && compressor()],
+  adapter,
   image: {
     service: {
       entrypoint: 'astro/assets/services/noop',
