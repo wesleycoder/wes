d40ace79a9f817a9a93b6f1323552212fd783792
Wésley Guimarães <wes@guima.dev>
2024-07-07 19:44:12 -0300

    feat: better bg

--- apps/blog/tailwind.config.mjs
+++ apps/blog/tailwind.config.mjs
@@ -6,6 +6,30 @@ export default {
       fontFamily: {
         drama: ['Aachen', 'serif'],
       },
+      backgroundSize: {
+        '200%': '200%',
+        '300%': '300%',
+        '400%': '400%',
+      },
+      keyframes: {
+        'bg-wave': {
+          '0%': {
+            'clip-path': 'polygon(15% 95%, 30% 05%, 45% 95%, 60% 05%, 75% 95%, 90% 05%)',
+            'background-position': '0% 50%',
+          },
+          '50%': {
+            'clip-path': 'polygon(15% 5%, 30% 95%, 45% 5%, 60% 95%, 75% 5%, 90% 95%)',
+            'background-position': '100% 50%',
+          },
+          '100%': {
+            'clip-path': 'polygon(15% 95%, 30% 05%, 45% 95%, 60% 05%, 75% 95%, 90% 05%)',
+            'background-position': '0% 50%',
+          },
+        },
+      },
+      animation: {
+        'bg-wave': 'bg-wave 30s linear infinite alternate',
+      },
     },
   },
   plugins: [],
